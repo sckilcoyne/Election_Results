9641acbaa021527bed6769ead6e3c943a3b13fa7
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2021-09-20 23:16:32 -0400

    start to import endorsements

--- voting2021app.py
+++ voting2021app.py
@@ -5,7 +5,15 @@ Created on Mon Sep 20 22:23:23 2021
 @author: Scott
 """
 import streamlit as st
+import os
+import requests
+import pandas as pd
 
+# %% Prep Things
+temp = '/tmp/'
+os.makedirs(temp, exist_ok=True)  # Make temp directory if needed
+
+# %% Layout
 st.title('Cambridge 2021 Election Voting Guide')
 
 # %% Preferences
@@ -21,6 +29,14 @@ equity = st.sidebar.slider('Social Equity', min_value=0, max_value=5, value=5)
 # %% Questions
 
 # %% Endorsements
+st.header('Endorsements')
+
+endorseGitHub = 'https://github.com/sckilcoyne/Election_Results/blob/main/endorsements.pkl?raw=true'
+
+r = requests.get(endorseGitHub, allow_redirects=True)
+tempFile = os.path.join(temp, 'endorsements.pkl')
+open(tempFile, 'wb').write(r.content)
+endorseDf = pd.read_pickle(endorseGitHub)
 
 # %% Candidate Scores and Voting Guide
 
