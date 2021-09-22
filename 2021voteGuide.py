4f35136928acbb6a24661badd0a77deb006dbcc2
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2021-09-21 21:05:47 -0400

    read data from google sheet and save for app use

--- 2021voteGuide.py
+++ 2021voteGuide.py
@@ -7,35 +7,38 @@ Created on Mon Sep 20 22:47:32 2021
 
 import pandas as pd
 
-endorsements = {
-    'Cambridge Bike Safety':
-        {'Bikes': True,
-         'Housing': False,
-         'Environment': True,
-         'Equity': False,
-         'Weight': 5},
-    'DSA':
-        {'Bikes': False,
-         'Housing': False,
-         'Environment': False,
-         'Equity': True,
-         'Weight': 2},
-    'A Better Cambridge':
-        {'Bikes': False,
-         'Housing': True,
-         'Environment': False,
-         'Equity': False,
-         'Weight': 3},
+
+# %% Functions
+def download_sheet_data():
+    googleSheet = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSouvpbGc1D3Q2KhTEAsqHH-LhnSs52N-TbOc351_WRt1Bb8OzmHAUWemlufR_NXbUGve5JNIjUbpcA/pub?output=xlsx'
+
+    guideData = pd.read_excel(googleSheet, sheet_name='Guide Data')
+
+    guideData.to_pickle('appDataFrames/guideData.pkl')
 
 
-}
+def import_saved_data():
+    return pd.read_pickle('appDataFrames/guideData.pkl')
 
 
-if __name__ == '__main__':
-    endorseDf = pd.DataFrame.from_dict(endorsements).transpose()
-    endorseDf['Bikes'].fillna(False, inplace=True)
-    endorseDf['Housing'].fillna(False, inplace=True)
-    endorseDf['Environment'].fillna(False, inplace=True)
-    endorseDf['Equity'].fillna(False, inplace=True)
+# %% Get Guide Data
+# download_sheet_data()
+guideData = import_saved_data()
 
-    endorseDf.to_pickle('endorsements.pkl')
+# %% Create Useable dataframes for app
+candidateScore = guideData[guideData['Category'].isna()].drop(
+    'Category', axis=1)
+
+data = guideData[guideData['Category'].notna()].drop(
+    ['First', 'Last', 'Incumbent'], axis=1).set_index('Category').transpose()
+
+questions = data[data['Type'] == 'Question']
+endorsements = data[data['Type'] == 'Endorsement']
+pledges = data[data['Type'] == 'Pledge']
+
+# %% Save Data to be used in app
+
+candidateScore.to_pickle('appDataFrames/candidateScore.pkl')
+questions.to_pickle('appDataFrames/questions.pkl')
+endorsements.to_pickle('appDataFrames/endorsements.pkl')
+pledges.to_pickle('appDataFrames/pledges.pkl')
