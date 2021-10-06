a115d411144fc2db2e9c22fc8eba9d07ce44de61
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2021-10-06 18:55:48 -0400

    big update to documentation and fixed scoring to normalize sub-topics

--- voting2021app.py
+++ voting2021app.py
@@ -24,7 +24,12 @@ widgetCount = 0
 
 
 def normalize(data):
-    return (data - data.min()) / (data.max() - data.min())
+    delta = data.max() - data.min()
+    if delta > 0:
+        norm = (data - data.min()) / delta
+    else:
+        norm = data.astype(bool).astype(int) * np.sign(data.min())
+    return norm
 
 
 # %% Layout
@@ -53,8 +58,64 @@ st.sidebar.write('Set weights of category preferences.')
 endorsementsWeight = st.sidebar.slider(
     'Endorsements', min_value=0, max_value=5, value=4)
 pledgesWeight = st.sidebar.slider('Pledges', min_value=0, max_value=5, value=3)
-questrionsWeight = st.sidebar.slider(
+questionsWeight = st.sidebar.slider(
     'Questions', min_value=0, max_value=5, value=2)
+
+# %% Intro
+with st.expander('How this works'):
+    '''
+    ## About this app
+    This is an app I created to help me figure out how to vote in the 2021 Cambridge City Council election. I have gathered and coded endorsements, pledges and questionnaire answers for all of the candidates in the race ([Google Doc](https://docs.google.com/spreadsheets/d/195MWS2K34Qgm7pJyC71Rh-lXdjmb8xoc6oKdPIvExlY/edit?usp=sharing)). If you think I am missing something, you can comment on the sheet. As this app was created to help me, the default weights reflect my personal pro-biking/housing/environment/equity views. You can view all of the poorly written source code on [GitHub](https://github.com/sckilcoyne/Election_Results).
+
+    ## About the Election
+    Cambridge City Council is composed of 9 seats with 2 year terms. All seats are at-large on the same ballot. Cambridge uses a form of Ranked Choice Voting called Single Transferable Vote, where you list candidates in order of preference and your top vote is counted until that candidate is eliminated, at which point your next non-eliminated candidate is voted for. 
+
+    ### More Detail About Cambridge’s RCV System
+    Votes are first counted by every ballot's first choice. In the first round,
+    if a candidate does not receive at least 50 votes, they are eliminated and
+    the ballots are redistributed to the remaining candidates based on the next
+    preference. Once a candidate reaches 10% of the total cast ballots + 1 vote
+    (reached the threshold), they are elected to the council. If an elected
+    candidate has more votes than the threshold, a random selection of ballots
+    equal to the votes excess of the threshold are redistributed based on next
+    eligible preference (yay Cambridge having non-deterministic elections!).
+    Every round the lowest ranked candidate is eliminated and their votes are
+    redistributed. This continues until 9 candidates exceed the threshold or
+    there are only 9 candidates remaining.
+
+    ## Voting Strategy
+    Reading the above RCV detail section, if you want your vote to count the most, you
+    should be ranking numerous candidates, even if your top few choices are
+    very likely to be elected, since you might have your ballot actually voting
+    for a lower preference.
+
+    In the elections since 2013 (last election with easily scrapable results),
+    incumbency leads to a huge re-election advantage. On average, only 1 incumbent
+    loses re-election per cycle (with 2 being the max). This suggests that the
+    optimal voting strategy is to first vote for your most prefered non-incumbent.
+
+    '''
+
+    githubRepo = 'https://raw.githubusercontent.com/sckilcoyne/Election_Results/'
+    githubURL = githubRepo + 'main/' + 'cambOutputs/'
+    st.markdown('![Subsequent Election Finishing Order](' + githubURL +
+                'Finishing%20Place%20in%20Subsequent%20Cycle.png)')
+
+    '''
+    ## About the Scoring
+    This whole system is basically a Pugh Matrix.
+    
+    On the sidebar, you can change the weights of different topics and types of data. The scale is from 0-5, with 5 being more preferred and 0 ignoring that aspect.
+    
+    In the “Weighting” expanders, you can see and adjust the weights of each endorsement/pledge/question. In the other expanders, you can see how each candidate is scored for each data point.
+    
+    The “Voting Guide” table lists the topic scores for each candidate and a combined score, which is a sum of the topic scores. 
+    
+    The topic scores are calculated for each topic and data type by multiplying the candidates’ score by the weight of the data point, then summing up their scores for each data point for that sub-type, e.g. all endorsements related to bikes or all answers related to housing. Then, all of these scores are normalized within that subtype so the highest scoring candidate gets a 1 and the lowest scoring candidate gets a 0 and everyone in between are scaled linearly. This 0 to 1 score is then multiplied by the type weighting, e.g. pledge weighting. Each type score is then added together and the candidate scores are normalized by the topic and multiplied by the topic weighting, e.g. environment weight.
+    
+    The sorting of the candidates is something I am still trying to improve. Right now, the first few non-incumbents are sorted above incumbents that may have a higher score because I think this will be a more optimal voting strategy. I am happy to get suggestions on how to better sort.
+    '''
+
 
 # %% Endorsements
 
@@ -135,43 +196,63 @@ endorsementsScores = candidatesDf[endorseDf.index.values] * endorseWeight
 pledgesScores = candidatesDf[pledgeDf.index.values] * pledgeWeight
 questionsScores = candidatesDf[questionDf.index.values] * questionWeight
 
-bikeScore = endorsementsWeight *\
-    endorsementsScores[endorseDf[endorseDf['Bikes'] == True].index.values].sum(axis=1) + \
-    pledgesWeight * \
-    pledgesScores[pledgeDf[pledgeDf['Bikes'] == True].index.values].sum(axis=1) + \
-    questrionsWeight * \
-    questionsScores[questionDf[questionDf['Bikes']
-                               == True].index.values].sum(axis=1)
+# Bike Scores
+bikeScoreEndorsement = endorsementsScores[endorseDf[endorseDf['Bikes'] == True].index.values].sum(
+    axis=1)
+bikeScorePledge = pledgesScores[pledgeDf[pledgeDf['Bikes'] == True].index.values].sum(
+    axis=1)
+bikeScoreAnswers = questionsScores[questionDf[questionDf['Bikes'] == True].index.values].sum(
+    axis=1)
+
+bikeScore = endorsementsWeight * normalize(bikeScoreEndorsement) + \
+    pledgesWeight * normalize(bikeScorePledge) + \
+    questionsWeight * normalize(bikeScoreAnswers)
 
 bikeScore = normalize(bikeScore) * bikeWeight
 
-housingScore = (endorsementsWeight *
-                endorsementsScores[endorseDf[endorseDf['Housing']
-                                             == True].index.values].sum(axis=1) +
-                pledgesWeight * pledgesScores[pledgeDf[pledgeDf['Housing']
-                                                       == True].index.values].sum(axis=1) +
-                questrionsWeight * questionsScores[questionDf[questionDf['Housing']
-                                                              == True].index.values].sum(axis=1))
+# Housing Scores
+housingScoreEndorsement = endorsementsScores[endorseDf[endorseDf['Housing'] == True].index.values].sum(
+    axis=1)
+housingScorePledge = pledgesScores[pledgeDf[pledgeDf['Housing'] == True].index.values].sum(
+    axis=1)
+housingScoreAnswers = questionsScores[questionDf[questionDf['Housing'] == True].index.values].sum(
+    axis=1)
+
+housingScore = endorsementsWeight * normalize(housingScoreEndorsement) + \
+    pledgesWeight * normalize(housingScorePledge) + \
+    questionsWeight * normalize(housingScoreAnswers)
+
 housingScore = normalize(housingScore) * housingWeight
 
-environmentScore = (endorsementsWeight *
-                    endorsementsScores[endorseDf[endorseDf['Environment']
-                                                 == True].index.values].sum(axis=1) +
-                    pledgesWeight * pledgesScores[pledgeDf[pledgeDf['Environment']
-                                                           == True].index.values].sum(axis=1) +
-                    questrionsWeight * questionsScores[questionDf[questionDf['Environment']
-                                                                  == True].index.values].sum(axis=1))
+# Environment Scores
+environmentScoreEndorsement = endorsementsScores[endorseDf[endorseDf['Environment'] == True].index.values].sum(
+    axis=1)
+environmentScorePledge = pledgesScores[pledgeDf[pledgeDf['Environment'] == True].index.values].sum(
+    axis=1)
+environmentScoreAnswers = questionsScores[questionDf[questionDf['Environment'] == True].index.values].sum(
+    axis=1)
+
+environmentScore = endorsementsWeight * normalize(housingScoreEndorsement) + \
+    pledgesWeight * normalize(housingScorePledge) + \
+    questionsWeight * normalize(housingScoreAnswers)
+
 environmentScore = normalize(environmentScore) * environmentWeight
 
-equityScore = (endorsementsWeight *
-               endorsementsScores[endorseDf[endorseDf['Equity']
-                                            == True].index.values].sum(axis=1) +
-               pledgesWeight * pledgesScores[pledgeDf[pledgeDf['Equity']
-                                                      == True].index.values].sum(axis=1) +
-               questrionsWeight * questionsScores[questionDf[questionDf['Equity']
-                                                             == True].index.values].sum(axis=1))
+# Equity Scores
+equityScoreEndorsement = endorsementsScores[endorseDf[endorseDf['Equity'] == True].index.values].sum(
+    axis=1)
+equityScorePledge = pledgesScores[pledgeDf[pledgeDf['Equity'] == True].index.values].sum(
+    axis=1)
+equityScoreAnswers = questionsScores[questionDf[questionDf['Equity'] == True].index.values].sum(
+    axis=1)
+
+equityScore = endorsementsWeight * normalize(housingScoreEndorsement) + \
+    pledgesWeight * normalize(housingScorePledge) + \
+    questionsWeight * normalize(housingScoreAnswers)
+
 equityScore = normalize(equityScore) * equityWeight
 
+# Combined Scores
 rawScore = bikeScore + housingScore + environmentScore + equityScore
 
 
@@ -205,42 +286,5 @@ st.dataframe(scoreDf)
 
 '''
 Voting guide shows highest scoring candidates up to the third incumbent, sorted 
-by non-incumbents first. See below notes for detailed explaination.
+by non-incumbents first. See intro notes for detailed explaination.
 '''
-
-
-# %% Incumbency
-with st.expander('Voting Strategy and Incumbency Notes'):
-
-    '''
-    Cambridge elects 9 council members every two years. All candidates are in
-    the same at-large race. Voting is done through a system of Ranked Choice
-    Voting (RCV) where every voter can order their preferred candidates.
-
-    Votes are first counted by every ballot's first choice. In the first round,
-    if a candidate does not receive at least 50 votes, they are eliminated and
-    the ballots are redistributed to the remaining candidates based on the next
-    preference. Once a candidate reaches 10% of the total cast ballots + 1 vote
-    (reached the threshold), they are elected to the council. If an elected
-    candidate has more votes than the threshold, a random selection of ballots
-    equal to the votes excess of the threshold are redistributed based on next
-    eligible preference (yay Cambridge having non-deterministic elections!).
-    Every round the lowest ranked candidate is eliminated and their votes are
-    redistributed. This continues until 9 candidates exceed the threshold or
-    there are only 9 candidates remaining.
-
-    Reading the above paragraph, if you want your vote to count the most, you
-    should be ranking numerous candidates, even if your top few choices are
-    very likely to be elected, since you might have your ballot actually voting
-    for a lower preference.
-
-    In the elections since 2013 (last election with easily scrapable results),
-    incumbency leads to a huge re-election advantage. On average, only 1 incumbent
-    loses re-election per cycle (with 2 being the max). This suggests that the
-    optimal voting strategy is to first vote for your most prefered non-incumbent.
-    '''
-
-    githubRepo = 'https://raw.githubusercontent.com/sckilcoyne/Election_Results/'
-    githubURL = githubRepo + 'main/' + 'cambOutputs/'
-    st.markdown('![Subsequent Election Finshing Order](' + githubURL +
-                'Finishing%20Place%20in%20Subsequent%20Cycle.png)')
