a4df4c8c118fe9e07b11b021241ef195cfbf5848
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2023-09-24 22:05:13 -0400

    updated how this works

--- voting2023app.py
+++ voting2023app.py
@@ -92,33 +92,60 @@ for topic in topics:
 st.sidebar.write('Set weights of category preferences.')
 
 endorsementsWeight = st.sidebar.slider(
-    'Endorsements', min_value=0, max_value=5, value=2)
+    'Endorsements', min_value=0, max_value=5, value=3)
 pledgesWeight = st.sidebar.slider(
-    'Pledges', min_value=0, max_value=5, value=4)
+    'Pledges', min_value=0, max_value=5, value=3)
 questionsWeight = st.sidebar.slider(
     'Questions', min_value=0, max_value=5, value=3)
 
 # %% Intro
+githubRepo = 'https://raw.githubusercontent.com/sckilcoyne/Election_Results/'
+githubURL = githubRepo + 'main/' + 'cambOutputs/'
+
 with tabIntro:
     '''
     ## About this app
-    This is an app I created to help me figure out how to vote in the Cambridge City Council election. I have gathered and coded endorsements, pledges and questionnaire answers for all of the candidates in the race ([Google Doc](https://docs.google.com/spreadsheets/d/1NzgAYpCYGcOoGnopR5xPZA474-S41oB0cxNsIzdB9Hw/edit?usp=sharing)). If you think I am missing something, you can comment on the sheet. As this app was created to help me, the default weights reflect my personal pro-biking/housing/environment/equity views. You can view all of the poorly written source code on [GitHub](https://github.com/sckilcoyne/Election_Results).
+    This is an app I created to help me figure out how to vote in the Cambridge City Council election.
+    I have gathered and coded endorsements, pledges and questionnaire answers for all of the candidates in the race
+    ([Google Doc](https://docs.google.com/spreadsheets/d/1NzgAYpCYGcOoGnopR5xPZA474-S41oB0cxNsIzdB9Hw/edit?usp=sharing)).
+    If you think I am missing something, you can comment on the sheet.
+    As this app was created to help me, the default weights reflect my personal pro-biking/housing/environment/equity views.
+    You can view all of the poorly written source code on [GitHub](https://github.com/sckilcoyne/Election_Results).
+    
+    I first created this system to help me understand the local politics of the city and be a better informed voter.
+    Going through this exercise has achieved those goals and led me to explicitly support ABC and CBS, both financially and with my time,
+    primarily responding to action alerts and giving comment to the council.
+    However, nothing in this guide reflects anyone's (including ABC or CBS) opinions other than mine.
 
     ## About the Scoring
     This whole system is basically a Pugh Matrix.
     
-    On the sidebar, you can change the weights of different topics and types of data. The scale is from 0-5, with 5 being more preferred and 0 ignoring that aspect.
+    On the sidebar, you can change the weights of different topics and types of data. 
+    The scale is from 0-5, with 5 being more preferred and 0 ignoring that aspect.
     
-    In the “Weighting” expanders, you can see and adjust the weights of each endorsement/pledge/question. In the other expanders, you can see how each candidate is scored for each data point.
+    In the “Weighting” expanders, you can see and adjust the weights of each endorsement/pledge/question. 
+    In the other expanders, you can see how each candidate is scored for each data point.
     
     The “Voting Guide” table lists the topic scores for each candidate and a combined score, which is a sum of the topic scores. 
     
-    The topic scores are calculated for each topic and data type by multiplying the candidates’ score by the weight of the data point, then summing up their scores for each data point for that sub-type, e.g. all endorsements related to bikes or all answers related to housing. Then, all of these scores are normalized within that subtype so the highest scoring candidate gets a 1 and the lowest scoring candidate gets a 0 and everyone in between are scaled linearly. This 0 to 1 score is then multiplied by the type weighting, e.g. pledge weighting. Each type score is then added together and the candidate scores are normalized by the topic and multiplied by the topic weighting, e.g. environment weight.
+    The topic scores are calculated for each topic and data type by multiplying the candidates’ score by 
+    the weight of the data point, then summing up their scores for each data point for that sub-type, 
+    e.g. all endorsements related to bikes or all answers related to housing. 
+    Then, all of these scores are normalized within that subtype so the highest scoring candidate gets 
+    a 1 and the lowest scoring candidate gets a 0 and everyone in between are scaled linearly. 
+    This 0 to 1 score is then multiplied by the type weighting, e.g. pledge weighting. 
+    Each type score is then added together and the candidate scores are normalized by the topic and 
+    multiplied by the topic weighting, e.g. environment weight.
     
-    The sorting of the candidates is something I am still trying to improve. Right now, the first few non-incumbents are sorted above incumbents that may have a higher score because I think this will be a more optimal voting strategy. I am happy to get suggestions on how to better sort.
+    The sorting of the candidates is something I am still trying to improve. 
+    Right now, the first few non-incumbents are sorted above incumbents that may have a higher score because 
+    I think this will be a more optimal voting strategy. I am happy to get suggestions on how to better sort.
     
     ## About the Election
-    Cambridge City Council is composed of 9 seats with 2 year terms. All seats are at-large on the same ballot. Cambridge uses a form of Ranked Choice Voting called Single Transferable Vote, where you list candidates in order of preference and your top vote is counted until that candidate is eliminated, at which point your next non-eliminated candidate is voted for. 
+    Cambridge City Council is composed of 9 seats with 2 year terms. All seats are at-large on the same ballot. 
+    Cambridge uses a form of Ranked Choice Voting called Single Transferable Vote, where you list candidates in 
+    order of preference and your top vote is counted until that candidate is eliminated, at which point your next 
+    non-eliminated candidate is voted for. 
 
     ### More Detail About Cambridge’s RCV System
     Votes are first counted by every ballot's first choice. In the first round,
@@ -141,19 +168,28 @@ with tabIntro:
 
     In the elections since 2013 (last election with easily scrapable results),
     incumbency leads to a huge re-election advantage. On average, only 1 incumbent
-    loses re-election per cycle (with 2 being the max). This suggests that the
-    optimal voting strategy is to first vote for your most prefered non-incumbent.
+    loses re-election per cycle (with 2 being the max). Further, we can look at how 
+    results in one election correlate to results in the subsequent election. Looking 
+    at finish place in one election to the next, first round vote totals from one 
+    election to the next, and to finishing place in the next election, we can see
+    that viable candidates generally improve upon their previous results, but 
+    even incumbents are not fully safe if they were one of the last few elected. 
+    This suggests the optimal voting strategy is to prioritize voting for candidates 
+    near the cut off.
+
+    For simplicity (since I haven't figured out a good way to integrate the below 
+    correlations yet), there is a slight de-rating of incumbents by default. If you
+    have a suggestion on a better method to take past performance into account, I'd 
+    love to discuss it.
 
     '''
 
-    githubRepo = 'https://raw.githubusercontent.com/sckilcoyne/Election_Results/'
-    githubURL = githubRepo + 'main/' + 'cambOutputs/'
     st.markdown('![Subsequent Election Finishing Order](' + githubURL +
                 'Finishing%20Place%20in%20Subsequent%20Cycle.png)')
-    
+
     st.markdown('![First Round Percentage of Threshold in Subsequent Cycle](' + githubURL +
                 'First%20Round%20Percentage%20of%20Threshold%20in%20Subsequent%20Cycle.png)')
-    
+
     st.markdown('![Subsequent Election Finishing Order](' + githubURL +
                 'First%20Round%20Percentage%20of%20Threshold%20vs.%20Finish%20in%20Subsequent%20Cycle.png)')
 
@@ -369,9 +405,9 @@ with tabVote:
 
     '''
     Voting guide shows highest scoring candidates, with some bias for 
-    non-incumbents. See intro notes for detailed explaination. You should play with 
+    non-incumbents. See "How this works" for detailed explaination. You should play with 
     the sliders because some of this data is pretty sparse (e.g. non-response of 
     questionaires, not all candidates wanting to get all endorsements they could get).
-    The data used also misses a bunch of nuance in the candidates (e.g. Mallon's 
-    focus on school meals), which is where the manual adjustments can compensate.
+    The data used also misses a bunch of nuance in the candidates, which is where the 
+    manual adjustments can compensate.
     '''
